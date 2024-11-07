1441ad44e6af4744b80f6549ce19d1cd9b874137
Maksym Syvash <maksym_syvash@outlook.com>
2024-11-08 00:08:11 +0200

    Add RGBA generic struct

---
+++ Lab1.Views/Colors/RGBA.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab1.Views.Colors;
+
+public struct RGBA<Component> where Component : struct
+{
+    public Component R { get; set; }
+    public Component G { get; set; }
+    public Component B { get; set; }
+    public Component A { get; set; }
+}
