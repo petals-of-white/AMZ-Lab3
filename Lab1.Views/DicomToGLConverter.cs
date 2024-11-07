5e27d3f95b57658d99f9a284394b24e95a51e0eb
Maksym Syvash <maksym_syvash@outlook.com>
2024-11-07 10:29:31 +0200

    Fix bit depth check

--- Lab1.Views/DicomToGLConverter.cs
+++ Lab1.Views/DicomToGLConverter.cs
@@ -3,9 +3,9 @@ using Lab1.Models;
 using static SharpGL.OpenGL;
 
 namespace Lab1.Views;
-public class DicomToGLConverter
+public class DicomToGLConverter(DicomManager dicom)
 {
-    private readonly DicomManager dicomManager;
+    private readonly DicomManager dicomManager = dicom;
 
     public uint InternalFormat => dicomManager.PhotometricInterpretation.Value switch
     {
@@ -24,17 +24,17 @@ public class DicomToGLConverter
 
         var i when i == PhotometricInterpretation.Rgb.Value => GL_RGB,
         var i when i == PhotometricInterpretation.Monochrome2.Value => GL_RED,
-        _ => throw new NotImplementedException("Other photometric interpretation is not implemented yet.")
+        _ => throw new NotImplementedException("Other photometric interpretations are not implemented yet.")
     };
     public uint Type => dicomManager.BitDepth switch
     {
-        { BitsStored: 8, IsSigned: true } => GL_BYTE,
-        { BitsStored: 8, IsSigned: false } => GL_UNSIGNED_BYTE,
-        { BitsStored: 16, IsSigned: true } => GL_SHORT,
-        { BitsStored: 16, IsSigned: false } => GL_UNSIGNED_SHORT,
-        { BitsStored: 32, IsSigned: true } => GL_INT,
-        { BitsStored: 32, IsSigned: false } => GL_UNSIGNED_INT,
+        { BitsStored: (> 0) and (<= 8), IsSigned: true } => GL_BYTE,
+        { BitsStored: (> 0) and (<= 8), IsSigned: false } => GL_UNSIGNED_BYTE,
+        { BitsStored: (<= 16), IsSigned: true } => GL_SHORT,
+        { BitsStored: (<= 16), IsSigned: false } => GL_UNSIGNED_SHORT,
+        { BitsStored: (<= 32), IsSigned: true } => GL_INT,
+        { BitsStored: (<= 32), IsSigned: false } => GL_UNSIGNED_INT,
+        // 12 bit => Розширити до GL_SHORT (16 bit)
         _ => throw new NotImplementedException("Other bit depth is not implemented")
     };
-    public DicomToGLConverter(DicomManager dicom) => dicomManager = dicom;
 }
