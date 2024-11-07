1bc3ae73c7ea1bd666e4d87745eb6be751a2b402
Maksym Syvash <maksym_syvash@outlook.com>
2024-11-08 00:10:05 +0200

    Completed ROITool abstract class

--- Lab1.Views/Tools/ROI/ROITool.cs
+++ Lab1.Views/Tools/ROI/ROITool.cs
@@ -1,21 +1,56 @@
-﻿namespace Lab1.Views.Tools.ROI;
-public abstract class ROITool(DicomGLViewer img) : IOverlayTool
+﻿using FellowOakDicom.Imaging.Mathematics;
+using Lab1.Views.Colors;
+using Lab1.Views.Graphics;
+using SharpGL;
+
+namespace Lab1.Views.Tools.ROI;
+
+public abstract class ROITool : IOverlayTool
 {
-    public DicomGLViewer ViewImage { get; } = img;
-    public bool IsShown { get; protected set; }
-    public bool IsROIActive { get; protected set; }
-    public virtual string ToolName => "Зона дослідження";
-    protected abstract void DisplayInfo();
-    protected abstract void DisplayRegion();
-    public void Show()
+    private readonly RegionOfInterestGL roiGL;
+
+    public ROITool(OpenGL gl)
     {
-        DisplayRegion();
-        DisplayInfo();
-        IsShown = true;
+        //ViewImage = img;;
+        roiGL = new(gl)
+        {
+            LineColor = new RGBA<float> { R = 1, G = 1, B = 0, A = 1 },
+            ReferencePointColor = new RGBA<float> { R = 1, G = 0, B = 0, A = 1 },
+        };
     }
+
+    public bool IsROIActive { get; protected set; }
+
+    public bool IsShown { get; protected set; }
+
+    public abstract uint PrimitiveType { get; }
+
+    public virtual string ToolName => "Зона дослідження";
+
+    //public DicomGLViewer ViewImage { get; }
+    protected abstract Point2D [] Contour { get; }
+
+    protected abstract Point2D [] ReferencePoints { get; }
+
     public void Hide()
     {
         IsShown = false;
-        throw new NotImplementedException();
     }
+
+    public void Show()
+    {
+        DisplayRegion();
+        //DisplayInfo();
+        IsShown = true;
+    }
+
+    public void UploadPoints()
+    {
+        roiGL.ReferencePoints = ReferencePoints;
+        roiGL.RegionContour = Contour;
+    }
+
+    protected void DisplayInfo() => throw new NotImplementedException();
+
+    protected void DisplayRegion() => roiGL.DrawRegionContour(PrimitiveType);
 }
