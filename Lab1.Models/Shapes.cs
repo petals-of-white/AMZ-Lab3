f1ed420999617b8cdf5d8b34bfb1760a9f93f08e
Maksym Syvash <maxim.sivash@gmail.com>
2024-11-13 14:15:46 +0200

    Added properties for Rectangle's Width and Height

--- Lab1.Models/Shapes.cs
+++ Lab1.Models/Shapes.cs
@@ -4,4 +4,8 @@ using System.Drawing;
 
 public record struct Ellipse(PointF Center, PointF R1, PointF R2);
 
-public record struct Rectangle(PointF P1, PointF P2);
+public record struct Rectangle(PointF P1, PointF P2)
+{
+    public readonly float Height => Math.Abs(P1.Y - P2.Y);
+    public readonly float Width => Math.Abs(P1.X - P2.X);
+}
