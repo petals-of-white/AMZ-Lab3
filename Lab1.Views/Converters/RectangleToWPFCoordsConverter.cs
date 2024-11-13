39d1738f6908f00d7d8600c352116e4ee9d58e08
Maksym Syvash <maxim.sivash@gmail.com>
2024-11-13 14:20:08 +0200

    Added converters for ROI visibility and rectangle coordinates

---
+++ Lab1.Views/Converters/RectangleToWPFCoordsConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Lab1.Models.Shapes;
+using Lab1.Views.Extensions;
+
+namespace Lab1.Views.Converters;
+
+[ValueConversion(typeof(Rectangle), typeof(double), ParameterType = typeof(WPFCoord))]
+public class RectangleToWPFCoordsConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var rectangle = (Rectangle) value;
+        var coord = (WPFCoord) parameter;
+        var topLeft = rectangle.TopLeft();
+        return coord switch
+        {
+            WPFCoord.Left => topLeft.X,
+            WPFCoord.Top => topLeft.Y,
+            _ => System.Windows.DependencyProperty.UnsetValue
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException("Can't convert from Left Value to Rectangle");
+    }
+}
+
+public enum WPFCoord
+{
+    Left, Top
+}
---
+++ Lab1.Views/Converters/VisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Lab1.Views.Converters;
+
+[ValueConversion(typeof(bool), typeof(Visibility))]
+public class VisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        bool isVisible = (bool) value;
+        if (isVisible) return Visibility.Visible;
+        else return Visibility.Hidden;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var visibility = (Visibility) value;
+
+        if (visibility == Visibility.Visible) return true;
+        else return false;
+    }
+}
