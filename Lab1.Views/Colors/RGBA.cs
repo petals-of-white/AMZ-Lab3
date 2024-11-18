3cb3259b0eb9f71ec7dd84c712c4ba9725eec293
Maksym Syvash <maxim.sivash@gmail.com>
2024-11-18 15:36:47 +0200

    Cleanup

--- Lab1.Views/Colors/RGBA.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab1.Views.Colors;
-
-public struct RGBA<Component> where Component : new()
-{
-    public Component R { get; set; }
-    public Component G { get; set; }
-    public Component B { get; set; }
-    public Component A { get; set; }
-}
